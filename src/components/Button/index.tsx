a782ef6f0c0420b2945e75c7680cb345f5f4dc0e
isnendyankp <isnendyankp@gmail.com>
2023-12-21 14:57:04 +0700

    Add interface Props with Label props with type string

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,9 @@
 
+interface Props {
+    Label: string;
+}
+
+
 const Button = () => {
     
   return (
@@ -8,3 +13,4 @@ const Button = () => {
 
 // Progress:
 // 1. cr8 Button component
+// 2. Add interface Props with Label props with type string
