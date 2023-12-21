6ac9e5d179f830802833cf7c4390784c2b9e8d36
isnendyankp <isnendyankp@gmail.com>
2023-12-21 14:13:54 +0700

    Add value border same as border-width: 1px from tailwind

--- src/containers/HomeContainer/index.tsx
+++ src/containers/HomeContainer/index.tsx
@@ -4,7 +4,7 @@ const HomeContainer = () => {
 
     return (
         <div>
-            <Input className="block border-neutral-400" />
+            <Input className="block border-neutral-400 border" />
         </div>
     )
 }
@@ -16,3 +16,4 @@ export default HomeContainer
 // 2. import input component
 // 3. render with pass in input component
 // 4. Add className props to input component with value display block and border neutral 400
+// 5. Add value border same as border-width: 1px from tailwind
