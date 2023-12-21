2475f2724e046a2f4cb462432a095bc9422305e1
isnendyankp <isnendyankp@gmail.com>
2023-12-21 12:27:16 +0700

    Add Comment info simple form

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      {/* Add onSubmit untuk cara baca all input text di dalam form */}
+      {/* Cr8 simple form with add onSubmit to controlling input text & button submit */}
       <form onSubmit={() => console.log('asd')}>
         <input type="text" onChange={() => console.log('asd')} />
         <input type="text" onChange={() => console.log('asd')} />
