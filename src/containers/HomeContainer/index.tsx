40f5104a2a5540af7b8518ffbfdfcf065c7acd03
isnendyankp <isnendyankp@gmail.com>
2023-12-21 13:44:42 +0700

    cr8 HomeContainer component

---
+++ src/containers/HomeContainer/index.tsx
@@ -0,0 +1,15 @@
+
+
+const HomeContainer = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HomeContainer
+
+// Progress:
+// 1. cr8 HomeContainer component
