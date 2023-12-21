cb725b47b102fa4709814b00ce831dcc31f2ea66
isnendyankp <isnendyankp@gmail.com>
2023-12-21 11:47:03 +0700

    create 5 input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 
-import { useState } from 'react';
 import './App.css'
 
 function App() {
   
 
   return (
-    const [name, setName] = useState();
-
     <>
-      {/* uncontrolled component */}
       <input type="text" onChange={() => console.log('asd')} />
-      {/* controlled component */}
-       <input type="text" onChange={() => console.log('asd')} value={name} />
+      <input type="text" onChange={() => console.log('asd')} />
+      <input type="text" onChange={() => console.log('asd')} />
+      <input type="text" onChange={() => console.log('asd')} />
+      <input type="text" onChange={() => console.log('asd')} />
     </>
-  )
+  );
 }
 
 export default App
