881705ab9c1533db4b08a4c7178c5dd8c76d5e91
isnendyankp <isnendyankp@gmail.com>
2023-12-21 15:12:03 +0700

    export button component for file distributor

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,9 @@
 
 export { default as Input } from './Input';
 export { default as Text } from './Text';
+export { default as Button } from './Button';
 
 // Progress:
 // 1. export input component for file distributor
 // 2. export text component for file distributor
+// 3. export button component for file distributor
