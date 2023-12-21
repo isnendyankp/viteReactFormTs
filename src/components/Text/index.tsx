cf76e0addf66ff45ace1a40db2590d5e9d74d27b
isnendyankp <isnendyankp@gmail.com>
2023-12-21 14:20:35 +0700

    Add children props/parameter with type Props to Text component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 
-const Text = () => {
+const Text = ({ children } : Props) => {
 
     return (
         <p></p>
@@ -18,3 +18,4 @@ export default Text;
 // Progress:
 // 1. cr8 Text component
 // 2. Cr8 type Props with children props with type string
+// 3. Add children props/parameter with type Props to Text component
