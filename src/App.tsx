073e8a38e80850516a9bfc6e723dfbde03f682cb
isnendyankp <isnendyankp@gmail.com>
2023-12-21 13:21:09 +0700

    test for check active tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,24 @@ function App() {
   
 
   return (
-    <>
-      {/* Cr8 simple form with add onSubmit to controlling input text & button submit */}
-      <form onSubmit={() => console.log('asd')}>
-        <input type="text" onChange={() => console.log('asd')} />
-        <input type="text" onChange={() => console.log('asd')} />
-        <input type="text" onChange={() => console.log('asd')} />
-        <input type="text" onChange={() => console.log('asd')} />
-        <input type="text" onChange={() => console.log('asd')} />
-        <button type="submit">Submit</button>
-      </form>
-    </>
+    <div className='bg-red-500'>
+      check active tailwind
+    </div>
   );
 }
 
 export default App
+
+/* 
+  Cr8 simple form with add onSubmit to controlling input text & button submit 
+   <>
+     <form onSubmit={() => console.log('asd')}>
+      <input type="text" onChange={() => console.log('asd')} />
+      <input type="text" onChange={() => console.log('asd')} />
+      <input type="text" onChange={() => console.log('asd')} />
+      <input type="text" onChange={() => console.log('asd')} />
+      <input type="text" onChange={() => console.log('asd')} />
+      <button type="submit">Submit</button>
+    </form>
+  </>
+*/
