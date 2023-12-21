d26118ba8cda87d50dd0c04946c2da7d4a5355cf
isnendyankp <isnendyankp@gmail.com>
2023-12-21 15:18:27 +0700

    Quick fix with add datatype button with input Props type

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 
-const Button = ({ label, ...props }) => {
+const Button = ({ label, ...props }: Props) => {
     
   return (
     <button {...props}>{label}</button>
@@ -19,4 +19,7 @@ export default Button;
 // 2. Add interface Props with Label props with type string
 // 3. add extends ButtonHTMLAttributes<HTMLButtonElement> to Props type
 // 4. Add label with type Props to Button component and spread it to button element
-// 5. Render label props/parameter to button element 
+// 5. Render label props/parameter to button element
+// 6. Add ...props to button element
+// 7. import ButtonHTMLAttributes from react
+// 8. Quick fix with add datatype button with input Props type 
