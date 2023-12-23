081f726714d004bca2b340b26773f5cd15937b72
isnendyankp <isnendyankp@gmail.com>
2023-12-23 16:06:00 +0700

    Add styling button with tailwind className props to button element

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,19 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes } from 'react';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
 }
 
-
 const Button = ({ label, ...props }: Props) => {
-    
   return (
-    <button {...props}>{label}</button>
+    <button
+      {...props}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+    >
+      {label}
+    </button>
   );
-}
+};
 
 export default Button;
 
@@ -22,4 +25,5 @@ export default Button;
 // 5. Render label props/parameter to button element
 // 6. Add ...props to button element
 // 7. import ButtonHTMLAttributes from react
-// 8. Quick fix with add datatype button with input Props type 
+// 8. Quick fix with add datatype button with input Props type
+// 9. Add styling button with tailwind className props to button element
